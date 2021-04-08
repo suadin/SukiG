45cf8c25c9553cbeae4d4d5fc4645760ba4b0bbd
suadin <betakiff@hotmail.de>
2021-04-08 13:36:26 +0200

    increase usability of chat
    * force scroll down by new messages

---
+++ src/Client/wwwroot/js/scroll.js
@@ -0,0 +1,9 @@
+﻿function scrollToFragment(elementId) {
+    var element = document.getElementById(elementId);
+
+    if (element) {
+        element.scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+}
